ed444181d18a9a60a9ba578df5615f91a6964d54
yeyee2901 <yhxzen2901@gmail.com>
2021-08-31 22:43:14 +0700

    Initial commit.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>yeyee2901 Web</title>
+</head>
+
+<body>
+  <h1>Welcome</h1>
+</body>
+
+</html>
