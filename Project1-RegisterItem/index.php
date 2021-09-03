1c9fb3e45653ce088eca3d624e7384cf3d3db5ab
yeyee2901 <yhxzen2901@gmail.com>
2021-09-03 16:45:50 +0700

    FINAL: fix html title.

--- Project1-RegisterItem/index.php
+++ Project1-RegisterItem/index.php
@@ -13,7 +13,7 @@ if (isset($_POST["search"])) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
-  <title>Form Registrasi Nama</title>
+  <title>Project 1 - Search</title>
 
   <!-- Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
